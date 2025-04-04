b31d793ef752641ebf67b5f5b53dc239358d8544
= <=>
2025-04-04 11:02:51 +0200

    Inital commit

---
+++ src/test/java/org/example/githubrepoapi/GitHubRepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubrepoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
